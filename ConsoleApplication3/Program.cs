885dd0aa433d51d02ecfcd3928b72360959fae38
vldik7 <vladmart1996@mail.ru>
2016-04-25 20:05:06 +0300

    no message

--- ConsoleApplication3/Program.cs
+++ ConsoleApplication3/Program.cs
@@ -9,69 +9,90 @@
     public class Program
     {
         private static Dictionary<string, string> colors;
+        private static HashSet<string> colorsUsed;
 
-        static string Method(Match match)
+        [SuppressMessage("StyleCop.CSharp.LayoutRules", "SA1503:CurlyBracketsMustNotBeOmitted", Justification = "Reviewed. Suppression is OK here.")]
+        public static string ColorsMethod(Match match)
         {
-            var s = match.Value.ToLower();
-            string replace = string.Empty;
-            if (s[0] == '#')
+            var matchValue = match.Value.ToLower();
+            var replace = string.Empty;
+            if (matchValue[0] == '#')
             {
-                if (s.Length == 7)
+                if (matchValue.Length == 7)
                 {
-                    return colors.ContainsKey(s) ? colors[s] : s;
+                    if (!colorsUsed.Contains(matchValue) && colors.ContainsKey(matchValue))
+                    {
+                        colorsUsed.Add(matchValue);
+                        File.AppendAllText(@"../../Data/colorUsed.txt", colors[matchValue] + "\n");
+                    }
+
+                    return colors.ContainsKey(matchValue) ? colors[matchValue] : matchValue;
+                }
+                   
+                replace += matchValue[0].ToString() + matchValue[1].ToString() + matchValue[1].ToString() + matchValue[2].ToString() + matchValue[2].ToString() + matchValue[3].ToString() + matchValue[3].ToString();
+                if (!colorsUsed.Contains(replace) && colors.ContainsKey(replace))
+                {
+                    colorsUsed.Add(replace);
+                    File.AppendAllText(@"../../Data/colorUsed.txt", colors[replace] + "\n");
+                }
+
+                return colors.ContainsKey(replace) ? colors[replace] : matchValue;
+            }
+
+            replace += '#';
+            var digit = string.Empty;
+            foreach (var Char in matchValue)
+            {
+                if (char.IsDigit(Char))
+                {
+                    digit += Char;
                 }
                 else
-                {                    
-                    replace += s[0].ToString() + s[1].ToString() + s[1].ToString() + s[2].ToString() + s[2].ToString() + s[3].ToString() + s[3].ToString();
-                    return colors.ContainsKey(replace) ? colors[replace] : s;
-                }
-            }
-            else
-            {
-                replace += '#';
-                var digit = string.Empty;
-                foreach (var c in s)
                 {
-                    if (char.IsDigit(c)) digit += c;
-                    else
+                    if (digit != string.Empty)
                     {
-                        if (digit != string.Empty)
+                        if (digit.Length == 1)
                         {
-                            if (digit.Length == 1)
-                            {
-                                replace += '0';
-                            }
-
-                            replace += Convert.ToString(Convert.ToInt32(digit), 16);
+                            replace += '0';
                         }
 
-                        digit = string.Empty;
+                        replace += Convert.ToString(Convert.ToInt32(digit), 16);
                     }
-                }
 
-                return colors.ContainsKey(replace) ? colors[replace] : s;
+                    digit = string.Empty;
+                }
             }
+
+            if (!colorsUsed.Contains(replace) && colors.ContainsKey(replace))
+            {
+                colorsUsed.Add(replace);
+                File.AppendAllText(@"../../Data/colorUsed.txt", colors[replace] + "\n");
+            }
+
+            return colors.ContainsKey(replace) ? colors[replace] : matchValue;
         }
 
         public static void Main()
         {
-            var path = "../../Data/colors.txt";
+            var pathColor = "../../Data/colors.txt";
+            File.WriteAllText(@"../../Data/colorUsed.txt","");
             colors = new Dictionary<string, string>();
-            using (var sr = File.OpenText(path))
+            colorsUsed = new HashSet<string>();
+            using (var streamReader = File.OpenText(pathColor))
             {
                 string s;
-                while ((s = sr.ReadLine()) != null)
+                while ((s = streamReader.ReadLine()) != null)
                 {
-                    var color = s.Split(null as string[], StringSplitOptions.RemoveEmptyEntries);
+                    var color = s.Split(new char[] { }, StringSplitOptions.RemoveEmptyEntries);
                     colors.Add(color[1].ToLower(), color[0]);
                 }
             }
 
-            var pat = @"rgb\((\d{1,3},\s*){2}\d{1,3}\)|#[a-f,0-9,A-F]{6}|#[a-f,0-9,A-F]{3}";
-            path = "../../Data/source.txt";
-            var text = File.ReadAllText(path);
-            text = Regex.Replace(text, pat, Method);
-            System.IO.File.WriteAllText(@"../../Data/output.txt", text);
+            var pattern = @"rgb\((\d{1,3},\s*){2}\d{1,3}\)|#[a-f,0-9,A-F]{6}|#[a-f,0-9,A-F]{3}";
+            var pathSource = "../../Data/source.txt";
+            var text = File.ReadAllText(pathSource);
+            text = Regex.Replace(text, pattern, ColorsMethod);
+            File.WriteAllText(@"../../Data/output.txt", text);
         }
     }
 }
